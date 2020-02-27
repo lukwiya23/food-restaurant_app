e7ee8452084ae896b470cb4c1fe8461a172f9243
lukwiya23 <luk23bonnie8@gmail.com>
2020-02-27 18:27:54 +0300

    __creating dummy data for food model__

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -11,3 +11,11 @@ final _pasta =
     Food(imageUrl: 'assets/images/pasta.jpg', name: 'Pasta', price: 5.99);
 final _ramen =
     Food(imageUrl: 'assets/images/ramen.jpg', name: 'Ramen', price: 4.99);
+final _pancakes =
+    Food(imageUrl: 'assets/images/pancakes.jpg', name: 'Pancakes', price: 9.99);
+final _burger =
+    Food(imageUrl: 'assets/images/burger.jpg', name: 'Burger', price: 14.99);
+final _pizza =
+    Food(imageUrl: 'assets/images/pizza.jpg', name: 'Pizza', price: 11.99);
+final _salmon = Food(
+    imageUrl: 'assets/images/salmon.jpg', name: 'Salmon Salad', price: 12.99);
