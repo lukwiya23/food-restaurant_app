9437fc80b68592b2bd156ff7fae55ef78ecec426
lukwiya23 <luk23bonnie8@gmail.com>
2020-02-28 18:31:42 +0300

    __data for the restaurant modes__

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -19,3 +19,10 @@ final _pizza =
     Food(imageUrl: 'assets/images/pizza.jpg', name: 'Pizza', price: 11.99);
 final _salmon = Food(
     imageUrl: 'assets/images/salmon.jpg', name: 'Salmon Salad', price: 12.99);
+
+    //Restaurants
+    final _retaurant0 = Restaurant(
+      imageUrl: "assets/images/restaurant0.jpg",
+      name: 'Cafe Monte',
+      
+    )
